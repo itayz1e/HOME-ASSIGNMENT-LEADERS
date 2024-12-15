54e3e00760a250e4955e6b4f4d96440aa4a031fd
itayz1e <itayz1e@gmail.com>
2024-12-15 15:15:22 +0200

    fix bug

--- client/src/interface.ts
+++ client/src/interface.ts
@@ -1,5 +1,5 @@
 export interface Influencer {
-  user_id: any;
+  user_id: string;
   fullname: string;
   username: string;
   followers: string;
@@ -18,4 +18,9 @@ export interface SearchBarProps {
 export interface ApiResponse {
   success: boolean;
   data: Influencer[];
+}
+
+export interface SearchResultsProps {
+  results: Influencer[];
+  onSelect: (influencer: Influencer) => void;
 }
